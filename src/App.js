21f7390decef7cb7466bb7cbea98bdb33045c447
RPDool <rp.dool@student.alfa-college.nl>
2025-05-08 13:48:48 +0200

    added projects and resume

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ function App() {
   const projects = [
     {
       id: 1,
-      title: "Project Title 1",
-      description: "A brief description of what this project does. Highlight any cool tech or challenges you solved."
+      title: "3D Game",
+      description: "This is my first 3d game project. Its a basic shooter. you can walk around and shoot",
+      image: "/3d-game.png", // Path to the image in the public folder
     },
-    {
-      id: 2,
-      title: "Project Title 2",
-      description: "Another project with a cool feature or tech stack explained.",
-    },
+    // {
+    //   id: 2,
+    //   title: "Project Title 2",
+    //   description: "Another project with a cool feature",
+    //   image: "/3d-game.png", // Path to the image in the public folder
+    // },
   ];
 
   return (
@@ -33,6 +35,7 @@ function App() {
           <a href="mailto:robindool07@gmail.com" className="email">
             robindool07@gmail.com
           </a>
+          <a href='https://robsnor.itch.io/' className="email" >robsnor.itch.io </a>
 
           <div className="social-icons">
             <a href="#"><i className="fab fa-github"></i></a>
@@ -59,7 +62,9 @@ function App() {
           <div className="project-list">
             {projects.map((project) => (
               <div className="project" key={project.id}>
-                <div className="project-img">IMG</div>
+                <div className="project-img">
+                  <img src={process.env.PUBLIC_URL + project.image} alt={project.title} />
+                </div>
                 <div>
                   <h3 className="project-title">{project.title}</h3>
                   <p>{project.description}</p>
@@ -69,7 +74,9 @@ function App() {
           </div>
         </section>
         <div className="resume-link">
-          <a href="#">Resume →</a>
+          <a href="/CV Robin Dool.pdf" download="Robin_Dool_Resume.pdf">
+            Resume →
+          </a>
         </div>
       </main>
     </div>
